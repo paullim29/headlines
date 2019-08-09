a29365357768103aa28a846889bc072a6e930b19
Paul <paullim29@gmail.com>
2019-08-09 16:13:27 +0800

    with Jinja templates

--- headlines.py
+++ headlines.py
@@ -1,10 +1,9 @@
 import feedparser
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
-
-BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 'cnn': 'http://rss.cnn.com/rss/edition.rss',
@@ -15,15 +14,8 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 @app.route("/<publication>")
 def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
-    return """<html>
-    <body>
-    <h1> BBC Headlines </h1>
-    <b>{0}</b> <br/>
-    <i>{1}</i> <br/>
-    <p>{2}</p> <br/>
-</body>
-</html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+    #first_article = feed['entries'][0]
+    return render_template("home.html", articles=feed['entries'])
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
